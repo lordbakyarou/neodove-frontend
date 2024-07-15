ca535567398a9646f80378f9c0fdf9e320317a0d
Mayur Hanwate <frostblizard111@gmail.com>
2024-07-15 08:27:38 +0530

    fnal added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import YourChats from "./YourChats";
 const Sidebar = () => {
   return (
     <div className="hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 z-[80] shadow-xl border-r">
-      <div className="flex flex-col gap-3 bg-white h-full">
+      <div className="flex flex-col gap-3 bg-white h-full h">
         <SidebarNavbar />
         <YourChats />
       </div>
